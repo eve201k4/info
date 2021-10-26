f71bb61fd8418c750ae8ebc71b0cb0fc5df90b38
eve201k4 <iff_k4@hotgmail.com>
2021-10-26 09:33:19 +0700

    test folder 1.0

--- folder01/index.php
+++ folder01/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World!!! Folder 01";
+echo "Hello World!!! Folder 01 <br>";
 echo "<a href='http://testweb01.us-east-2.elasticbeanstalk.com/' target='_parent'>Back Index/</a>";
 
 
