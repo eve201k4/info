25d90dcfaff15d526a1adccef4a4d68db159bb0a
eve201k4 <iff_k4@hotgmail.com>
2021-10-27 17:02:04 +0700

    test index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ header ('Content-type: text/html; charset=utf-8');
 $cdate_full = date("Y-m-d H:i:s"); 
 echo "cdate_full = ".$cdate_full."<br>";
 echo "Hello World!!! 01<br>";
-echo "<a href='./folder01/' target='_parent'>folder01/</a>";
+echo "<a href='./folder01/' target='_parent'>folder01/</a><br>";
+echo "<a href='./connect.php' target='_parent'>connect.php/</a><br>";
 
 ?>
