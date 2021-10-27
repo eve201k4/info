f231422abcbd122ee96a762134b120514b9271b7
eve201k4 <iff_k4@hotgmail.com>
2021-10-27 16:40:56 +0700

    test Index

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+date_default_timezone_set('Asia/Bangkok');
+header ('Content-type: text/html; charset=utf-8');
+
+$cdate_full = date("Y-m-d H:i:s"); 
+echo "cdate_full = ".$cdate_full."<br>";
 echo "Hello World!!! 01<br>";
 echo "<a href='./folder01/' target='_parent'>folder01/</a>";
 
-  
 ?>
