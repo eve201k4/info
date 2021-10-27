85d73686b7ca25fccca99069c06d9e8962669f37
eve201k4 <iff_k4@hotgmail.com>
2021-10-27 16:58:52 +0700

    connect DB

--- connect.php
+++ connect.php
@@ -7,18 +7,17 @@ session_start();
 date_default_timezone_set('Asia/Bangkok');
 header ('Content-type: text/html; charset=utf-8');
 
-
 include('include/adodb5/adodb.inc.php');
 include('folder01/var.inc.php');
 
 $db = adoNewConnection('mysqli'); # eg. 'mysqli' or 'oci8'
 $db->debug = true;
-$db->connect('localhost', 'root', 'root', 'game');
+$db->connect($hostDB, $userDB, $passDB, $nameDB);
 //$rs = $db->execute('select * from bioderma_2018');
 
 echo "test";
 print "<pre>";
-//print_r($rs->getRows());
+print_r($rs->getRows());
 print "</pre>";
 
 echo "var_folder01 = ".$var_folder01."<br>";
